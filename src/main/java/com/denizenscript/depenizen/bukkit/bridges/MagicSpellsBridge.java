caf64504245de85ce6c047fd24a00b1cc4de237e
MCSamuel <morrissamuelw@gmail.com>
2026-03-16 08:08:23 -0700

    Added <magicspells.list_spells>

--- src/main/java/com/denizenscript/depenizen/bukkit/bridges/MagicSpellsBridge.java
+++ src/main/java/com/denizenscript/depenizen/bukkit/bridges/MagicSpellsBridge.java
@@ -1,13 +1,42 @@
 package com.denizenscript.depenizen.bukkit.bridges;
 
-import com.denizenscript.depenizen.bukkit.events.magicspells.*;
-import com.denizenscript.depenizen.bukkit.properties.magicspells.MagicSpellsPlayerProperties;
-import com.denizenscript.depenizen.bukkit.Bridge;
 import com.denizenscript.denizen.objects.PlayerTag;
 import com.denizenscript.denizencore.events.ScriptEvent;
+import com.denizenscript.denizencore.objects.core.ElementTag;
+import com.denizenscript.denizencore.objects.core.ListTag;
 import com.denizenscript.denizencore.objects.properties.PropertyParser;
+import com.denizenscript.denizencore.tags.PseudoObjectTagBase;
+import com.denizenscript.denizencore.tags.TagManager;
+import com.denizenscript.depenizen.bukkit.Bridge;
+import com.denizenscript.depenizen.bukkit.events.magicspells.*;
+import com.denizenscript.depenizen.bukkit.properties.magicspells.MagicSpellsPlayerProperties;
+import com.nisovin.magicspells.MagicSpells;
 
 public class MagicSpellsBridge extends Bridge {
+
+    static class MagicSpellsTagBase extends PseudoObjectTagBase<MagicSpellsTagBase> {
+
+        public static MagicSpellsTagBase instance;
+
+        public MagicSpellsTagBase() {
+            instance = this;
+            TagManager.registerStaticTagBaseHandler(MagicSpellsTagBase.class, "magicspells", (t) -> instance);
+        }
+
+        public void register() {
+
+            // <--[tag]
+            // @attribute <magicspells.list_spells>
+            // @returns ListTag
+            // @plugin Depenizen, MagicSpells
+            // @description
+            // Returns a list of all registered spells.
+            // -->
+            tagProcessor.registerTag(ListTag.class, "list_spells", (attribute, object) -> {
+                return new ListTag(MagicSpells.spells(), spell -> new ElementTag(spell.getName(), true));
+            });
+        }
+    }
 
     @Override
     public void init() {
@@ -16,5 +45,6 @@ public class MagicSpellsBridge extends Bridge {
         ScriptEvent.registerScriptEvent(SpellCastedScriptEvent.class);
         ScriptEvent.registerScriptEvent(ManaChangeScriptEvent.class);
         ScriptEvent.registerScriptEvent(SpellLearnScriptEvent.class);
+        new MagicSpellsTagBase();
     }
 }
